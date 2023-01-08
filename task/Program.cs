45ec4e2202af0d1917cd281c3a3798d0ef3cdcf5
Vera <mvp2283@mail.ru>
2023-01-08 19:07:59 +0600

    count <=3

---
+++ task/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам.
+*/
+
+string[] array1 = new string[5] {"1234", "23", "hello", "world", "-2"};
+string[] array2 = new string[array1.Length];
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArray(array1, array2);
+PrintArray(array2);
